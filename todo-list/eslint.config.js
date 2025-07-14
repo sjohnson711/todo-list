b28605b8d354100acc2bb82b5e12824e8f83bd80
Seth Johnson <samaritanbrotherseth@gmail.com>
2025-07-14 19:34:29 -0400

    updated the eslintg to the proper portion

--- todo-list/eslint.config.js
+++ todo-list/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import globals from 'globals';
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -9,6 +10,9 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
@@ -23,9 +27,10 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: { react: {version: 'detect'} },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-
+      'react/prop-types': 'off'
     },
   },
   
