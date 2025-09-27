689a4d95b47e13a238ecadc8fb19c2d26338659d
Seth <samaritanbrotherseth@gmail.com>
2025-09-27 19:08:25 -0400

    fixed the spelling of browser router

--- todo-list/src/main.jsx
+++ todo-list/src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowswerRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowswerRouter>
+    <BrowserRouter>
       <App />
-    </BrowswerRouter>
+    </BrowserRouter>
   </StrictMode>
 );
